fe6fc9592cd7df6a9384c24aafe1188f9a4a13c3
nttlong <zugeliang2000@gmail.com>
2022-11-13 11:13:37 +0700

    ok

--- test_apps/controllers/application/__init__.py
+++ test_apps/controllers/application/__init__.py
@@ -5,5 +5,5 @@ from test_apps.web_models.apps import AppInfo
 class ApplicationInfo(pydantic.BaseModel):
     Name:str
 @cy_web.hanlder("post","apps")
-def list_of_apps(code:str,token=cy_web.auth())->List[int]:
+def list_of_apps(app:AppInfo,token=cy_web.auth())->List[int]:
     return [1,2,3]
